6fdf04e9f893c28cd88a4715450246203010da0c
tekenscodr <ensleybdarku@gmail.com>
2024-02-13 21:21:36 +0000

    add

--- app/components/container/Container.tsx
+++ app/components/container/Container.tsx
@@ -5,13 +5,13 @@ import Item from './Item'
 
 type Props = {}
 
+
 const Container = (props: Props) => {
   return (
     <div className='mb-[200px]'>
       <div className='flex '>
         <Link href='/filters' className='opacity-60'>
           <div>
-            {/* <Filter/> */}
           </div>
         </Link>
         <div className='px-20'>
